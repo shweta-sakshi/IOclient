5eae6d94ebad4836636470a9d1ff5e0af10e1853
shweta-sakshi <shweta25sakshi@gmail.com>
2024-08-19 00:04:45 +0530

    update import link

--- src/component/Sellernavbar.jsx
+++ src/component/Sellernavbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { FaBars, FaBell, FaSearch, FaUserCircle } from 'react-icons/fa'
 
 const Sellernavbar = ({ sidebarToggle, setSidebarToggle }) => {
--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import { FaHome, FaListAlt, FaShoppingCart, FaCog } from "react-icons/fa";
 
 const Sidebar = ({ sidebarToggle }) => {
